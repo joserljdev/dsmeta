19826c5bce4fd85b1d07159fd4a620c7d08fe84f
José Ronaldo Leles Júnior <joserljdev@gmail.com>
2022-07-23 14:29:48 -0300

    Selecao por data

--- backend/src/main/java/br/com/joserldev/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/com/joserldev/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package br.com.joserldev.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joserldev.dsmeta.entities.Sale;
@@ -17,8 +18,10 @@ public class SaleController {
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return this.service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return this.service.findSales(minDate, maxDate, pageable);
 	}
-
 }
